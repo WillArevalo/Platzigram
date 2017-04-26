f39aa34d5bba15260939951c705fc398d98c3e3a
Will Arevalo <willi11-95@hotmail.com>
2017-04-26 16:40:59 -0500

    Agrego botones de subida
    De foto y cancelar. con funciones
    class.toggle para agregar o quitar clases de un elemento.

--- src/homepage/template.js
+++ src/homepage/template.js
@@ -1,10 +1,23 @@
 var yo = require('yo-yo');
 var layout = require('../layout');
 var picture = require('../picture-card');
+var translate = require('../translate');
 
 module.exports = function (pictures) {
   var el = yo`<div class="container timeline">
     <div class="row">
+      <div class="col s12 m10 offset-m1 l8 offset-l2 center-align">
+        <form enctype="multipart/form-data" class="form-upload" id="formUpload">
+          <div id="fileName" class="fileUpload btn btn-flat cyan">
+            <span><i class="fa fa-camera" aria-hidden="true"></i>${translate.message('upload-picture')}</span>
+            <input name="picture" id="file" type="file" class="upload" onchange=${onchange} />
+          </div>
+          <button id="btnUpload" type="submit" class="btn btn-flat cyan hide">${translate.message('upload')}</button>
+          <button id="btnCancel" class="btn btn-flat red hide" onclick=${cancel}><i class="fa fa-times" aria-hidden="true"></i></button>
+        </form>
+      </div>
+    </div>
+    <div class="row">    
       <div class="col s12 m10 offset-m1 l6 offset-l3">
         ${pictures.map(function (pic) {
           return picture(pic);
@@ -12,6 +25,21 @@ module.exports = function (pictures) {
       </div>
     </div>
   </div>`;
+
+  function toggleButtons(){
+    document.getElementById('fileName').classList.toggle('hide'); //agrega o quita una clase si existe
+    document.getElementById('btnUpload').classList.toggle('hide');
+    document.getElementById('btnCancel').classList.toggle('hide');
+  }
+
+  function onchange() {
+    toggleButtons();
+  }
+
+  function cancel(){
+    toggleButtons();
+    document.getElementById('formUpload').reset();
+  }
 
   return layout(el);
 }
